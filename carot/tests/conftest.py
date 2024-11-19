dd7b4bb9c71dc4a274c7789a17ebf101f978ec24
Alice Driessen <adr@zurich.ibm.com>
2024-11-19 13:27:17 +0100

    Added training tests

--- carot/tests/conftest.py
+++ carot/tests/conftest.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
 import pytest
+import os
 from cmonge.utils import load_config
 
 from carot.datasets.conditional_loader import ConditionalDataModule
 from carot.datasets.single_loader import CarModule
+from carot.trainers.conditional_monge_trainer import ConditionalMongeTrainer
+from cmonge.trainers.ot_trainer import MongeMapTrainer
 
 
 @pytest.fixture(scope="session")
@@ -31,3 +34,39 @@ def synthetic_data(synthetic_config):
 def cond_synthetic_data(cond_synthetic_config):
     module = ConditionalDataModule(cond_synthetic_config.data)
     return module
+
+
+def test_conditional_model_training(cond_synthetic_config):
+
+    config = load_config(cond_synthetic_config)
+
+    datamodule = ConditionalDataModule(config.data, config.condition)
+
+    logger_path = Path(config.logger_path)
+
+    datamodule = ConditionalDataModule(config.data, config.condition)
+    trainer = ConditionalMongeTrainer(
+        jobid=1, logger_path=logger_path, config=config.model, datamodule=datamodule
+    )
+
+    trainer.train(datamodule)
+    trainer.evaluate(datamodule)
+
+    os.remove(logger_path)
+
+
+def test_model_training(synthetic_config):
+
+    config = load_config(synthetic_config)
+
+    datamodule = ConditionalDataModule(config.data, config.condition)
+
+    logger_path = Path(config.logger_path)
+
+    datamodule = MongeMapTrainer(config.data, config.condition)
+    trainer = MongeMapTrainer(jobid=1, logger_path=logger_path, config=config.model)
+
+    trainer.train(datamodule)
+    trainer.evaluate(datamodule)
+
+    os.remove(logger_path)
